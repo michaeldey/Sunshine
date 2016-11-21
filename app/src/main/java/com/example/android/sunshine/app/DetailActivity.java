ce0d1e32481c9ee7170271862bab50cd1c6e6356
michaeldey <michaeldey@hotmail.com>
2016-11-21 12:02:15 -0700

    Lesson 3 - added settings dropdown to clicked intent file

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -31,7 +31,7 @@ public class DetailActivity extends ActionBarActivity{
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // inflate the menu; this adds items to the action bar if it is present
-        //getMenuInflater().inflate(R.id.detail, menu); //save for later?
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
         return true;
     }//end of onCreateOptionsMenu
 
